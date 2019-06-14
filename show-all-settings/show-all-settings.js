706ab4713742105a79cb0da1f2c46877be67aba2
Bjoern <bjohas@users.noreply.github.com>
2019-06-14 21:24:34 +0100

    Update show-all-settings.js

--- show-all-settings/show-all-settings.js
+++ show-all-settings/show-all-settings.js
@@ -1,20 +1,22 @@
-var x = document.getElementsByClassName("_board_settings_link");
-if (x) {
-  var settings = x[0];
-  var url = settings.href;
-  url = url.replace("settings","");
-  addSetting("automation_rules","A");
-  addSetting("powerups","P");
-  addSetting("workflow","W");
-  addSetting("card_template","C");
-  addSetting("card_types","T");
-}
+function showAllSettings() {
+  var x = document.getElementsByClassName("_board_settings_link");
+  if (x) {
+    var settings = x[0];
+    var url = settings.href;
+    url = url.replace("settings","");
+    addSetting(settings,url,"automation_rules","A");
+    addSetting(settings,url,"powerups","P");
+    addSetting(settings,url,"workflow","W");
+    addSetting(settings,url,"card_template","C");
+    addSetting(settings,url,"card_types","T");
+  }
+};
 
-function addSetting(ext,letter) {
+function addSetting(insertionPt,uu,ext,letter) {
   var el = document.createElement('a');
   el.innerHTML = letter;
   el.classList.add('kt-right-pane-element');
   el.classList.add('_board_settings_link');
-  el.href = url + ext;
-  settings.parentNode.insertBefore(el, settings.nextSibling);
+  el.href = uu + ext;
+  insertionPt.parentNode.insertBefore(el, insertionPt.nextSibling);
 };
